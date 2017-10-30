df02db34b0b8f16c46ce9e4a0468e8d7f4535010
Jesse Skeets <jesseskeets@gmail.com>
2017-10-30 15:39:27 -0700

    Tutorial complete fully functioning dapp

---
+++ petshop_tutorial/readme.md
@@ -0,0 +1,3 @@
+# Pet Shop Dapp
+ # Built using truffle, and solidity. 
+ # Followed tutorial  <http://truffleframework.com/tutorials/pet-shop>
--- petshop_tutorial/test/TestAdoption.sol
+++ petshop_tutorial/test/TestAdoption.sol
@@ -10,4 +10,28 @@ import "../contracts/Adoption.sol";
 
 contract TestAdoption {
     Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+    // Testing the adopt() function
+    function testUserCanAdoptPet() {
+        uint returnedId = adoption.adopt(8);
+        uint expected = 8;
+        Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
+    }
+
+    function testGetAdopterAddressByPetId() {
+        // Expected owner is this contract
+        address expected = this;
+        address adopter = adoption.adopters(8);
+        Assert.equal(adopter, expected, "Owner of pet ID 8 should be recorded.");
+    }
+
+    // Testing retrieval of all pet owners
+    function testGetAdopterAddressByPetIdInArray() {
+        // Expected owner is this contract
+        address expected = this;
+        // Store adopters in memory rather than in contract's storage
+        address[16] memory adopters = adoption.getAdopters();
+
+        Assert.equal(adopters[8],expected, "Owner of pet ID 8 should be recorded.");
+    }
 }
